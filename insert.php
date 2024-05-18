73857ed548dd173e18a37ac57c1d5ce726c3a38c
jayashri-1107 <jayashripatil658@gmail.com>
2024-05-18 16:16:38 +0530

    change border

--- insert.php
+++ insert.php
@@ -10,13 +10,13 @@
 <center>
 <form method ="post" action="process.php">
 
-<table border="2">
-<tr><td>name</td><td><input type="text" name="uname" placeholder="enter user name" required></td></tr>
-<tr><td>password</td><td><input type="password" name="password" placeholder="enter password" required></td></tr>
-<tr><td>gender</td></tr>
-<tr><td>male</td><td><input type="radio" name="gender" value="male"></td></tr>
-<tr><td>female</td><td><input type="radio" name="gender" value="female"></td></tr>
-<tr><td>education :</td></tr>
+<table border="3">
+<tr><td>Name</td><td><input type="text" name="uname" placeholder="enter user name" required></td></tr>
+<tr><td>Password</td><td><input type="password" name="password" placeholder="enter password" required></td></tr>
+<tr><td>Gender</td></tr>
+<tr><td>Male</td><td><input type="radio" name="gender" value="male"></td></tr>
+<tr><td>Female</td><td><input type="radio" name="gender" value="female"></td></tr>
+<tr><td>Education :</td></tr>
 <tr><td><input type="checkbox" name="education[]" value="10th"><td>10th</td></td></tr>
 <tr><td><input type="checkbox" name="education[]" value="12th"><td>12th</td></td></tr>
 <tr><td><input type="checkbox" name="education[]" value="b.tech"><td>b.tech</td></td></tr>
